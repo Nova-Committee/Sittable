f3cf09b663250c14169948828c75208ef1ae4c4c
MikhailTapio <mikhailtapio13468@163.com>
2023-05-02 19:25:17 +0800

    Split sitting logic

--- src/main/java/committee/nova/sittable/common/event/handler/ForgeEventHandler.java
+++ src/main/java/committee/nova/sittable/common/event/handler/ForgeEventHandler.java
@@ -1,16 +1,11 @@
 package committee.nova.sittable.common.event.handler;
 
-import committee.nova.sittable.common.entity.impl.SittableEntity;
 import committee.nova.sittable.common.event.impl.SittableRegisterEvent;
 import committee.nova.sittable.common.registry.type.SittableRegistry;
-import committee.nova.sittable.common.sittable.Messages;
 import committee.nova.sittable.common.util.Utilities;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,23 +15,9 @@ public class ForgeEventHandler {
     @SubscribeEvent
     public static void onInteract(PlayerInteractEvent.RightClickBlock e) {
         final Level level = e.getWorld();
-        final BlockHitResult hit = e.getHitVec();
+        if (level.isClientSide) return;
         final BlockPos pos = e.getPos();
-        final BlockState state = level.getBlockState(pos);
-        final SittableRegistry registry = SittableRegisterEvent.getSittables().get(state.getBlock());
-        if (registry == null) return;
-        final double height = registry.height().applyAsDouble(state);
-        if (height == Double.MIN_VALUE) return;
-        e.setCanceled(true);
-        final Player player = e.getPlayer();
-        if (Utilities.isOccupied(level, pos)) {
-            player.displayClientMessage(Messages.OCCUPIED.getComponent(), true);
-            return;
-        }
-        final SittableEntity sittable = new SittableEntity(level, pos.getX() + .5, pos.getY() + height, pos.getZ() + .5);
-        level.addFreshEntity(sittable);
-        if (player.isPassenger()) player.stopRiding();
-        player.startRiding(sittable);
+        e.setCanceled(Utilities.trySit(level, pos, level.getBlockState(pos), e.getPlayer()));
     }
 
     @SubscribeEvent
--- src/main/java/committee/nova/sittable/common/util/Utilities.java
+++ src/main/java/committee/nova/sittable/common/util/Utilities.java
@@ -2,9 +2,14 @@ package committee.nova.sittable.common.util;
 
 import com.google.common.collect.ImmutableList;
 import committee.nova.sittable.common.entity.impl.SittableEntity;
+import committee.nova.sittable.common.event.impl.SittableRegisterEvent;
+import committee.nova.sittable.common.registry.type.SittableRegistry;
+import committee.nova.sittable.common.sittable.Messages;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Pose;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -30,5 +35,21 @@ public class Utilities {
     public static boolean isOccupied(Level level, BlockPos pos) {
         return !level.getEntitiesOfClass(SittableEntity.class, new AABB(pos.getX() + .0625, pos.getY(), pos.getZ() + .0625,
                 pos.getX() + .9375, pos.getY() + 1.5, pos.getZ() + .9375)).isEmpty();
+    }
+
+    public static boolean trySit(Level level, BlockPos pos, BlockState state, Player player) {
+        final SittableRegistry registry = SittableRegisterEvent.getSittables().get(state.getBlock());
+        if (registry == null) return false;
+        final double height = registry.height().applyAsDouble(state);
+        if (height == Double.MIN_VALUE) return false;
+        if (Utilities.isOccupied(level, pos)) {
+            player.displayClientMessage(Messages.OCCUPIED.getComponent(), true);
+            return true;
+        }
+        final SittableEntity sittable = new SittableEntity(level, pos.getX() + .5, pos.getY() + height, pos.getZ() + .5);
+        level.addFreshEntity(sittable);
+        if (player.isPassenger()) player.stopRiding();
+        player.startRiding(sittable);
+        return true;
     }
 }
