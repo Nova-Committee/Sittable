5b923055858d40fc8c86f086fd2b1ae74392f954
MikhailTapio <mikhailtapio13468@163.com>
2023-05-06 13:39:43 +0800

    Client Glitch

--- src/main/java/committee/nova/sittable/common/event/handler/ForgeEventHandler.java
+++ src/main/java/committee/nova/sittable/common/event/handler/ForgeEventHandler.java
@@ -6,7 +6,6 @@ import committee.nova.sittable.common.util.Utilities;
 import net.minecraft.block.BlockSlab;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -20,18 +19,15 @@ public class ForgeEventHandler {
 
     @SubscribeEvent
     public void onInteract(PlayerInteractEvent.RightClickBlock e) {
-        final World w = e.getWorld();
-        if (w.isRemote) return;
-        e.setCanceled(Utilities.trySit(w, e.getPos(), e.getFace(), e.getEntityPlayer()));
+        e.setCanceled(Utilities.trySit(e.getWorld(), e.getPos(), e.getFace(), e.getEntityPlayer()));
     }
 
     @SubscribeEvent
     public void onSittableRegister(SittableRegisterEvent e) {
-        if (Utilities.isInDevEnv()) e.registerSittable(new SittableRegistry(Blocks.WOODEN_SLAB,
-                (s, p, f) -> {
-                    if (p.isSneaking() || ((BlockSlab) s.getBlock()).isDouble() || s.getValue(BlockSlab.HALF).equals(BlockSlab.EnumBlockHalf.TOP))
-                        return Optional.empty();
-                    return Optional.of(new Vec3d(.5, .5, .5));
-                }));
+        if (Utilities.isInDevEnv()) e.registerSittable(new SittableRegistry(Blocks.WOODEN_SLAB, (s, p, f) -> {
+            if (p.isSneaking() || ((BlockSlab) s.getBlock()).isDouble() || s.getValue(BlockSlab.HALF).equals(BlockSlab.EnumBlockHalf.TOP))
+                return Optional.empty();
+            return Optional.of(new Vec3d(.5, .5, .5));
+        }));
     }
 }
