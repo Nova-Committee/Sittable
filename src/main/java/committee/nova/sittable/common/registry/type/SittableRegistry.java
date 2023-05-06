2e98b05afa050936a792c020ed93a87aa2772fcb
MikhailTapio <mikhailtapio13468@163.com>
2023-05-06 14:34:28 +0800

    Add doc

--- src/main/java/committee/nova/sittable/common/registry/type/SittableRegistry.java
+++ src/main/java/committee/nova/sittable/common/registry/type/SittableRegistry.java
@@ -9,6 +9,14 @@ import java.util.Optional;
 public class SittableRegistry {
     @FunctionalInterface
     public interface OffsetStrategy {
+        /**
+         * @param block    The block to check if it's sittable
+         * @param metadata The block's metadata
+         * @param player   The player interacting with the block
+         * @param face     The clicked face of the block
+         * @return An {@link Optional} of a {@link Vec3}. Its value represents the offset of the player's actual sitting position
+         * from the block's location. If it returns an {@link Optional#empty()}, it means that the block is not sittable in this case.
+         */
         Optional<Vec3> apply(Block block, int metadata, EntityPlayer player, int face);
     }
 
